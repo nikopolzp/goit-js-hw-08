a17a5646f8dc41f1bff7ed168e8e471cad64c748
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-02-03 18:59:23 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,11 +12,33 @@ form.addEventListener('input', throttle(elem =>
 }, 500));
 
 form.addEventListener('submit', elem => {
-   elem.preventDefault();
-console.log({ email: email.value, message: message.value });
-form.reset();
-localStorage.removeItem(STORAGE_KEY);
+   elem.preventDefault(); // отмена обновления страницы
+    console.log({ email: email.value, message: message.value });
+    //вывод в консоль значений
+    form.reset();
+//очистка полей формы
+    localStorage.removeItem(STORAGE_KEY);
+// очистка хранилища
 });
+
+const load = key => {
+    tru 
+    {
+        const serializedState = localStorage.getItem(key);
+        // localStorage.getItem() - возвращает значение ключа 
+        //если его нет null
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+         } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+        
+const storage = load(STORAGE_KEY);
+if (storage) {
+  email.value = storage.email;
+  message.value = storage.message;
+}
+
 
 
 
