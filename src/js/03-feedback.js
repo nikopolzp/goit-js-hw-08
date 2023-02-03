9f712c4413009afa5ff77ffeca5239f723c28be3
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-02-03 18:24:38 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import { throttle } from 'lodash';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const masseqe = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(elem =>
+{
+    const objectToSave = { email: email.value, message: message.value };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(objectToSave));
+}, 500));
+
+form.addEventListener('submit', elem => {
+   elem.preventDefault();
+console.log({ email: email.value, message: message.value });
+form.reset();
+localStorage.removeItem(STORAGE_KEY);
+});
+
+
+
 
