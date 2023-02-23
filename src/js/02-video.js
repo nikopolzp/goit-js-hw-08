818befb0adc5ab6c51de5b7ba08e92c3a16e4e7b
Ruslan P <111646522+nikopolzp@users.noreply.github.com>
2023-02-23 20:27:25 +0200

    update  task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,13 @@
-
 import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( elem => {
+player.on(
+  'timeupdate',
+  throttle(elem => {
     localStorage.setItem('videoplayer-current-time', elem.seconds);
-    }, 1000)
-    );
-
-/*
-player
-    .setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
-     console.error(error)
-    // an error occurred setting the color
-});
-*/
+  }, 1000)
+);
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-
